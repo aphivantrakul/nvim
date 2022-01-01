75c469c50eccb8d32439fd3ccb1fe7029fbb2986
Paul Aphivantrakul <aphivantrakul@gmail.com>
2022-01-02 00:00:18 +0700

    automatically download Packer if not present in computer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,73 +1,30 @@
-require('packer').startup(function()
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+
+  use 'norcalli/nvim-colorizer.lua'
 
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
 
-  --use 'rmehri01/onenord.nvim'
-
-  --use 'EdenEast/nightfox.nvim'
-
-  --use 'Mofiqul/vscode.nvim'
-
-  --use 'marko-cerovac/material.nvim'
-
-  --use "projekt0n/github-nvim-theme"
-
-  --use 'yashguptaz/calvera-dark.nvim'
-
-  --use({
-  --  "catppuccin/nvim",
-  --  s = "catppuccin"
-  --})
-
-  --use 'titanzero/zephyrium'
-
   use 'aphivantrakul/nvcode-color-schemes.vim'
 
-  use 'norcalli/nvim-colorizer.lua'
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 require('plugin_configs')
 
---require('onenord').setup({
---  theme = 'dark',
---})
-
---require('nightfox').load('nightfox')
-
---vim.g.vscode_style = "dark"
---vim.cmd[[colorscheme vscode]]
-
---vim.g.material_style = "deep ocean"
---vim.cmd 'colorscheme material'
-
---require('github-theme').setup()
-
---require('calvera').set()
-
---vim.cmd[[colorscheme catppuccin]]
-
---require 'zephyrium'
-
-require('nvim-treesitter.configs').setup({
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { "c", "rust" },  -- list of language that will be disabled
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    keymaps = {
-      open = 'gtd' -- Opens the playground for current buffer (if applicable)
-    },
-    updatetime = 25 -- Debounced time for highlighting nodes in the playground from source code
-  }
-})
+require'colorizer'.setup()
 
 vim.cmd[[colorscheme iceberg]]
-
-require'colorizer'.setup()
