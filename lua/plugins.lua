ddf9deeb1420d98290b7b85de603f50284e27e88
Paul Aphivantrakul <aphivantrakul@gmail.com>
2022-01-02 14:20:57 +0700

    Remove automatic install of Packer for now because automatic install does not work

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,4 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
-require('packer').startup(function(use)
+require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
   use 'norcalli/nvim-colorizer.lua'
@@ -17,14 +11,8 @@ require('packer').startup(function(use)
   use 'aphivantrakul/nvcode-color-schemes.vim'
 
   use 'ctrlpvim/ctrlp.vim'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
 
 require('plugin_configs')
 
-require'colorizer'.setup()
+require('colorizer').setup()
